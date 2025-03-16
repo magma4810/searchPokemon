fbe101b35767c5321facdf1812c7b099217500e9
Pavel <magma4810@gmail.com>
2025-03-17 00:36:06 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
-import { store } from "./example/store/index.ts";
+import { store } from "./store/index.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
